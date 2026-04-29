41503d4662a342801d16d089b1d031342664250a
bubul.com <amindalshad42@gmail.com>
2026-04-29 08:33:50 +0200

    andra commit

--- main.js
+++
@@ -1,7 +0,0 @@
-await import("./intro.js");
-await import("./demographics.js");
-await import("./lifestyle.js");
-await import("./stress.js");
-await import("./risk.js");
-await import("./performance.js");
-await import("./conclusion.js");
